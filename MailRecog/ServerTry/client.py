6e4ca7e0aa551e57c06671f2818fb3ea788e11fe
Atekkey <anand@tekkey.net>
2025-03-01 21:16:40 -0600

    small change

--- MailRecog/ServerTry/client.py
+++ MailRecog/ServerTry/client.py
@@ -59,13 +59,7 @@ while True:
     ret, frame = cap.read()
     if not ret:
         print("Failed to grab frame")
-        break
-
-    cv2.imshow('USB Camera', frame)
-
-    # Press 'q' to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # break
 
 cap.release()
 cv2.destroyAllWindows()
