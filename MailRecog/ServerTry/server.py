1c440fb2d7fb54dc019b54b0b7856b14b23d0bad
Atekkey <anand@tekkey.net>
2025-03-01 22:06:55 -0600

    Working, small changes

--- MailRecog/ServerTry/server.py
+++ MailRecog/ServerTry/server.py
@@ -16,9 +16,12 @@ print("Server is listening...")
 client_socket, client_address = server_socket.accept()
 print(f"Connection from {client_address} accepted")
 cap = cv2.VideoCapture(0) # use 0
-
+i = -1
 while True:
+    i+=1
     ret, frame = cap.read()
+    if(i %3 !=0):
+        break # Speed up attempt
     frame_data = pickle.dumps(frame)
     client_socket.sendall(struct.pack("Q", len(frame_data)))
     client_socket.sendall(frame_data)
